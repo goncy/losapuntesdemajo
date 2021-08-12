ec5916f3d089ef04841697054970ab8a7fadc363
goncy <gonzalo.pozzo4@gmail.com>
2021-08-12 20:23:52 -0300

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from "next/app";
+import Image from "next/image";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import styles from "../styles/Home.module.scss";
+
+import "../styles/globals.css";
+
+function App({Component, pageProps}: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Powered by{" "}
+          <span className={styles.logo}>
+            <Image alt="Vercel Logo" height={16} src="/vercel.svg" width={72} />
+          </span>
+        </a>
+      </footer>
+    </>
+  );
 }
-export default MyApp
+export default App;
