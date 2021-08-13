134cc344c18890de00037a96eb3e3274af57d4ac
Alexis Guzman <alexis12989x@gmail.com>
2021-08-13 10:39:18 -0500

    feat(theme): set up chakra provider
    import chkra provider component and theme file

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '@/styles/theme';
+
 const MyApp = ({ Component, pageProps }) => {
-	return <Component {...pageProps} />;
+	return (
+		<ChakraProvider theme={theme} resetCSS>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
 };
 
 export default MyApp;
