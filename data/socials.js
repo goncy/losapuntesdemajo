06cb06447589c27c41a50c00d06c89b9b89b88c6
Alexis Guzman <alexis12989x@gmail.com>
2021-08-13 17:49:48 -0500

    feat: create socials object file

---
+++ data/socials.js
@@ -0,0 +1,24 @@
+import { GithubIcon, InstagramIcon, TwitterIcon } from '@/styles/theme/icons';
+
+const socials = [
+	{
+		icon: TwitterIcon,
+		name: 'Twitter',
+		href: 'https://twitter.com/MajoLedes/',
+		color: '#1da1f2',
+	},
+	{
+		icon: GithubIcon,
+		name: 'GitHub',
+		href: 'https://github.com/majoledesma/',
+		color: 'currentColor',
+	},
+	{
+		icon: InstagramIcon,
+		name: 'Instagram',
+		href: 'https://www.instagram.com/los.apuntesdemajo/',
+		color: '#C13584',
+	},
+];
+
+export default socials;
