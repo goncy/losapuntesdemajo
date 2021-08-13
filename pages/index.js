b4d0f14d776823a48c466e092e012b48ab0b1bf2
Alexis Guzman <alexis12989x@gmail.com>
2021-08-13 17:50:22 -0500

    feat(home): set up home page component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,27 @@
-const index = () => {
+import { Box } from '@chakra-ui/react';
+
+import notes from '@/data/notes.json';
+
+import Header from '@/components/Header';
+import Notes from '@/components/Notes';
+import BuyMe from '@/components/BuyMe';
+import Footer from '@/components/Footer';
+import ToggleThemeButton from '@/components/ToggleThemeButtom';
+
+export const getStaticProps = async () => {
+	return { props: { notes } };
+};
+
+const index = ({ notes }) => {
 	return (
-		<div>
-			<p>Home Page Component</p>
-		</div>
+		<Box as='main'>
+			<Header />
+			<Notes notes={notes} />
+			<BuyMe />
+			<Footer />
+
+			<ToggleThemeButton />
+		</Box>
 	);
 };
 
