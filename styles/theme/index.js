cab6e57a131d12c4cac90d3941845bbd09f28cb3
Alexis Guzman <alexis12989x@gmail.com>
2021-08-14 10:19:39 -0500

    feat(theme): set up font files and import on index theme file

--- styles/theme/index.js
+++ styles/theme/index.js
@@ -4,11 +4,12 @@ import { extendTheme } from '@chakra-ui/react';
 import styles from './styles';
 
 // Foundational styles overrides
+import fonts from './foundations/fonts';
 
 // Component styles overrides
 import Heading from './components/heading';
 
-const overrides = { styles, components: { Heading } };
+const overrides = { styles, fonts, components: { Heading } };
 
 const theme = extendTheme(overrides);
 
